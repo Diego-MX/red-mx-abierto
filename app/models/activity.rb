39b4f8d7acdf212fdf191c13e00f12c3c524313f
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-23 13:12:54 -0500

    added relationships between the checklist implementation guide tables

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,5 @@
 class Activity < ActiveRecord::Base
+  belongs_to :step
+  has_many :user_activities
+  has_many :users, through: :user_activities
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,4 @@
 class Step < ActiveRecord::Base
+  belongs_to :stage
+  has_many :activities
 end
