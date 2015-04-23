a0c2295bd30da8d0c2d460368a0f58ec134e276f
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-23 16:50:29 -0500

    stages and activities can now be successfully created

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,3 +1,17 @@
 class Stage < ActiveRecord::Base
   has_many :steps
+
+  def save_and_add_new_steps(new_steps=[])
+    # create stage and check if there
+    # are any new_steps to save
+    if self.save && new_steps.any?
+      new_steps.each { |st| create_step(st) }
+    end
+  end
+
+  private
+
+  def create_step(step_name)
+    self.steps.create(name: step_name)
+  end
 end
