e7fba14e2b97ee0a86bfa1c0b3703b7e8b545e31
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-23 18:09:12 -0500

    refactor a little bit the stage resource controller

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,5 +1,6 @@
 class StagesController < ApplicationController
   layout 'admin'
+  before_action :set_stage, only: [:edit, :update]
 
   def index
     @stages = Stage.all
@@ -12,13 +13,28 @@ class StagesController < ApplicationController
   def create
     @stage = Stage.new(stage_params)
     if @stage.save_and_add_new_steps(steps_params)
-      redirect_to stages_path
+      redirect_to stages_path, notice: I18n.t('stages.notices.saved_successfully')
     else
-      redirect_to new_stage_path
+      redirect_to new_stage_path, alert: I18n.t('stages.alerts.save_failed')
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @stage.update_attributes(stage_params)
+      redirect_to stages_path, notice: I18n.t('stages.notices.saved_successfully')
+    else
+      redirect_to new_stage_path, alert: I18n.t('stages.alerts.save_failed')
     end
   end
 
   private
+
+  def set_stage
+    @stage ||= Stage.find(params[:id])
+  end
 
   def stage_params
     params.require(:stage).permit(:name, :position)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :pages, only: :home
-  resources :stages, only: [:index, :new, :create]
-  # resources :steps, only: [:new, :create]
+  resources :stages, only: [:index, :new, :create, :edit, :update]
+  resources :steps, only: [:edit, :update]
   # resources :activities, only: [:new, :create]
 
   get 'pages/home'
