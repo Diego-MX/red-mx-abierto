146a43557cc496608c69fb821c699c2941e32dfd
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-23 21:15:22 -0500

    refactor all the stage and steps code logic to work better with rails philosophy

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,6 +1,6 @@
 class StagesController < ApplicationController
-  layout 'admin'
   before_action :set_stage, only: [:edit, :update]
+  layout 'admin'
 
   def index
     @stage = Stage.new
@@ -16,7 +16,7 @@ class StagesController < ApplicationController
     if @stage.save(stage_params)
       redirect_to stages_path, notice: I18n.t('stages.notices.saved_successfully')
     else
-      redirect_to new_stage_path, alert: I18n.t('stages.alerts.save_failed')
+      redirect_to stages_path, alert: I18n.t('stages.alerts.save_failed')
     end
   end
 
@@ -27,7 +27,7 @@ class StagesController < ApplicationController
     if @stage.update_attributes(stage_params)
       redirect_to stages_path, notice: I18n.t('stages.notices.saved_successfully')
     else
-      redirect_to new_stage_path, alert: I18n.t('stages.alerts.save_failed')
+      redirect_to stages_path, alert: I18n.t('stages.alerts.save_failed')
     end
   end
 
