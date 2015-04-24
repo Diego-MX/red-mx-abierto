accd203829ed2f845cf6ce39066704e2f896ddac
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-24 09:33:39 -0500

    - Clean translations and removed new and edit actions from stages and steps. - Added the activities section to the admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :pages, only: :home
-  resources :stages, only: [:index, :new, :create, :edit, :update] do
-    resources :steps, only: [:index, :new, :create, :edit, :update] do
-      resources :activities, only: [:index, :new, :create, :edit, :update]
+  resources :stages, only: [:index, :create, :update] do
+    resources :steps, only: [:index, :create, :update] do
+      resources :activities, only: [:index, :create, :update]
     end
   end
-  # resources :activities, only: [:new, :create]
 
   get 'pages/home'
 
