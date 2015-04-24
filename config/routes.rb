189c19af8bfcc19e0aaea596e28a99f9acf99276
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-24 10:01:30 -0500

    Created a admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :pages, only: :home
-  resources :stages, only: [:index, :create, :update] do
-    resources :steps, only: [:index, :create, :update] do
-      resources :activities, only: [:index, :create, :update]
+  namespace :admin do
+    resources :stages, only: [:index, :create, :update] do
+      resources :steps, only: [:index, :create, :update] do
+        resources :activities, only: [:index, :create, :update]
+      end
     end
   end
 
