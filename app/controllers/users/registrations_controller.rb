0057957d6fbd75f945ac2a6865b9ad9026333433
mikesaurio <mikesaurio@gmail.com>
2015-04-25 12:43:19 -0500

    acomodan los estados

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     def update_sanitized_params
        devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name)}
 
-
+puts params[:action]
         if params[:user] [:government_type] == 'delagacion'
           @delegacional_cheked = 'true'
         elsif params[:user] [:government_type] == 'municipal'
@@ -17,37 +17,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
         @deputation= ['Alvaro Obregon' ,  'Azcapotzalco','Benito Juarez', 'Cuajuimalpa', 'Coyoacan', 'Cuahutemoc', 'Gustavo A Madero', 'Iztacalco',
           'Iztapalapa', 'Magdalena Contreras', 'Miguel Hidalgo', 'Milpa Alta', 'Tlahuac', 'Tlalpan', 'Venustiano Carranza', 'Xochimilco']
-        @states= ['Aguascalientes',
-'Baja California Norte', 
-'Baja California Sur ',
-'Campeche ',
-'Chiapas ',
-'Chihuahua', 
-'Coahuila',
-'Colima ',
-'Durango', 
-'Guanajuato', 
-'Guerrero  ',
-'Hidalgo ',
-'Guadalajara',
-'Edo. México ',
-'Michoacán ',
-'Morelos ',
-'Nayarit',
-'Monterrey',
-'Oaxaca', 
-'Puebla ',
-'Querétaro', 
-'Quintana Roo', 
-'San Luís Potosí',
-'Sinaloa ',
-'Sonora ',
-'Tabasco', 
-'Tamaulipas', 
-'Tlaxcala ',
-'Veracruz',
-'Yucatán ',
-'Zacatecas',
-'Distrito Federal' ]
+        @states= ['Aguascalientes','Baja California Norte', 'Baja California Sur ','Campeche ','Chiapas ','Chihuahua', 'Coahuila','Colima ', 'Durango', 'Guanajuato', 
+                          'Guerrero  ', 'Hidalgo ','Guadalajara','Edo. México ','Michoacán ','Morelos ','Nayarit','Monterrey','Oaxaca', 'Puebla ','Querétaro', 'Quintana Roo', 
+                        'San Luís Potosí', 'Sinaloa ', 'Sonora ', 'Tabasco',  'Tamaulipas', 'Tlaxcala ', 'Veracruz', 'Yucatán ', 'Zacatecas', 'Distrito Federal' ]
     end
+
 end
