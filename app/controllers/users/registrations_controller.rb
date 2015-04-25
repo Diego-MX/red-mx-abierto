c830b2c43324710e116941ecb342a70b8fa2326d
mikesaurio <mikesaurio@gmail.com>
2015-04-25 12:36:03 -0500

    se agrega dropdawns estaticos

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,8 +5,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def update_sanitized_params
        devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name)}
-      puts '***********************'
-       puts params[:user] [:government_type]
+
+
         if params[:user] [:government_type] == 'delagacion'
           @delegacional_cheked = 'true'
         elsif params[:user] [:government_type] == 'municipal'
@@ -14,5 +14,40 @@ class Users::RegistrationsController < Devise::RegistrationsController
         elsif params[:user] [:government_type] == 'estatal'
           @estatal_cheked = 'true'
         end
+
+        @deputation= ['Alvaro Obregon' ,  'Azcapotzalco','Benito Juarez', 'Cuajuimalpa', 'Coyoacan', 'Cuahutemoc', 'Gustavo A Madero', 'Iztacalco',
+          'Iztapalapa', 'Magdalena Contreras', 'Miguel Hidalgo', 'Milpa Alta', 'Tlahuac', 'Tlalpan', 'Venustiano Carranza', 'Xochimilco']
+        @states= ['Aguascalientes',
+'Baja California Norte', 
+'Baja California Sur ',
+'Campeche ',
+'Chiapas ',
+'Chihuahua', 
+'Coahuila',
+'Colima ',
+'Durango', 
+'Guanajuato', 
+'Guerrero  ',
+'Hidalgo ',
+'Guadalajara',
+'Edo. México ',
+'Michoacán ',
+'Morelos ',
+'Nayarit',
+'Monterrey',
+'Oaxaca', 
+'Puebla ',
+'Querétaro', 
+'Quintana Roo', 
+'San Luís Potosí',
+'Sinaloa ',
+'Sonora ',
+'Tabasco', 
+'Tamaulipas', 
+'Tlaxcala ',
+'Veracruz',
+'Yucatán ',
+'Zacatecas',
+'Distrito Federal' ]
     end
 end
