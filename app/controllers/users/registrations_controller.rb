1586ae745d8b43bf1cd9573c6259f84e2bc33fd7
mikesaurio <mikesaurio@gmail.com>
2015-04-27 17:19:01 -0500

    se valida el tipo de gobierno

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,78 +1,78 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-before_filter :set_values
+module Devise
+  class Users::RegistrationsController < Devise::RegistrationsController
+    before_filter :set_values
 
-   def new
-    @user = User.new
-  end
-  
-  def create
-    unless params[:user].nil?
-        tipo_funcionario
-    end 
-    @user = User.new(user_params) 
-    if @user.save 
-      redirect_to new_user_registration_path,  alert:  "Usuario ya existe"
-    else
-      render :new
+    def new
+      @user = User.new
     end
-  end
-  
-  private
-  
-  def user_params
-    params.require(:user).permit(:email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name)
-  end
-
-  def set_values
-    @deputations= ['Alvaro Obregon' ,  'Azcapotzalco','Benito Juarez', 'Cuajuimalpa', 'Coyoacan', 'Cuahutemoc', 'Gustavo A Madero', 'Iztacalco',
-    'Iztapalapa', 'Magdalena Contreras', 'Miguel Hidalgo', 'Milpa Alta', 'Tlahuac', 'Tlalpan', 'Venustiano Carranza', 'Xochimilco']
-
-    @states= ['Aguascalientes','Baja California Norte', 'Baja California Sur ','Campeche ','Chiapas ','Chihuahua', 'Coahuila','Colima ', 'Durango', 'Guanajuato', 
-      'Guerrero  ', 'Hidalgo ','Guadalajara','Edo. México ','Michoacán ','Morelos ','Nayarit','Monterrey','Oaxaca', 'Puebla ','Querétaro', 'Quintana Roo', 
-      'San Luís Potosí', 'Sinaloa ', 'Sonora ', 'Tabasco',  'Tamaulipas', 'Tlaxcala ', 'Veracruz', 'Yucatán ', 'Zacatecas', 'Distrito Federal' ]
-  end
-
-
-
-  def tipo_funcionario
-    if params[:user][:government_type] == 'delagacion'
-      @delegacional_cheked = 'true'
-    elsif params[:user][:government_type] == 'municipal'
-      @municional_cheked = 'true'
-    elsif params[:user][:government_type] == 'estatal'
-      @estatal_cheked = 'true'
+    
+    def create
+      unless params[:user].nil?
+        type_of_officer
+        @user = User.new(user_params) 
+        if @user.save 
+          redirect_to new_user_registration_path,  notice:  "Usuario registrado"
+        else
+          render :new
+        end
+      end 
     end
-  end
+    
+    private
+    
+    def user_params
+      params.require(:user).permit(:email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name)
+    end
 
+    def set_values
+      @deputations= ['Alvaro Obregon' ,  'Azcapotzalco','Benito Juarez', 'Cuajuimalpa', 'Coyoacan', 'Cuahutemoc', 'Gustavo A Madero', 'Iztacalco',
+        'Iztapalapa', 'Magdalena Contreras', 'Miguel Hidalgo', 'Milpa Alta', 'Tlahuac', 'Tlalpan', 'Venustiano Carranza', 'Xochimilco']
 
-  def all_params
-  unless params[:user][:password].blank?
-     @password = params[:user][:password]
+        @states= ['Aguascalientes','Baja California Norte', 'Baja California Sur ','Campeche ','Chiapas ','Chihuahua', 'Coahuila','Colima ', 'Durango', 'Guanajuato', 
+          'Guerrero  ', 'Hidalgo ','Guadalajara','Edo. México ','Michoacán ','Morelos ','Nayarit','Monterrey','Oaxaca', 'Puebla ','Querétaro', 'Quintana Roo', 
+          'San Luís Potosí', 'Sinaloa ', 'Sonora ', 'Tabasco',  'Tamaulipas', 'Tlaxcala ', 'Veracruz', 'Yucatán ', 'Zacatecas', 'Distrito Federal' ]
+        end
+
+        def type_of_officer
+          @type_government = params[:user][:government_type]
+          if @type_government  == 'delagacion'
+            @delegacional_cheked = 'true'
+          elsif @type_government  == 'municipal'
+            @municional_cheked = 'true'
+          elsif @type_government  == 'estatal'
+            @estatal_cheked = 'true'
+          end
+        end
+
+        def all_params
+          unless params[:user][:password].blank?
+           @password = params[:user][:password]
+         end
+         unless params[:user][:government_type].blank?
+           @government_type = params[:user][:government_type]
+         end
+         unless params[:user][:state].blank?
+           @state = params[:user][:state]
+         end
+         unless params[:user][:municipality].blank?
+           @municipality = params[:user][:municipality]
+         end
+         unless params[:user][:deputation].blank?
+          @deputation = params[:user][:deputation]
+        end
+        unless params[:user][:dependency_name].blank?
+         @dependency_name = params[:user][:dependency_name]
+       end
+       unless params[:user][:full_name].blank?
+         @full_name = params[:user][:full_name]
+       end
+       unless params[:user][:email].blank?
+         @email = params[:user][:email]
+       end
+       if !@password.blank? && !@government_type.blank? && !@dependency_name.blank? && !@full_name.blank? && !@email.blank? 
+         return true 
+       end
+       return false
+     end
    end
-   unless params[:user][:government_type].blank?
-     @government_type = params[:user][:government_type]
-   end
-   unless params[:user][:state].blank?
-     @state = params[:user][:state]
-   end
-   unless params[:user][:municipality].blank?
-     @municipality = params[:user][:municipality]
-   end
-   unless params[:user][:deputation].blank?
-    @deputation = params[:user][:deputation]
-   end
-   unless params[:user][:dependency_name].blank?
-     @dependency_name = params[:user][:dependency_name]
-   end
-   unless params[:user][:full_name].blank?
-     @full_name = params[:user][:full_name]
-   end
-   unless params[:user][:email].blank?
-     @email = params[:user][:email]
-   end
-   if !@password.blank? && !@government_type.blank? && !@dependency_name.blank? && !@full_name.blank? && !@email.blank? 
-     return true 
-   end
-   return false
- end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-
-  validates_presence_of :email, :message => 'Debes escribir tu correo.', on:  :create
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "El correo debe tener un formato válido.", on:  :create
-
-  validates_presence_of :password, :message => 'Debes escribir un password.', on:  :create
-  validates_length_of :password, :minimum => 8, :message => 'El password debe tener por lo menos 8 caracteres.', on:  :create
 
   validates_presence_of :dependency_name, :message => 'Debes escribir el nombre de la dependencia', on:  :create
   validates_length_of :dependency_name, :minimum => 10, :message => 'El nombre de la dependencia debe tener por lo menos 3 caracteres.', on:  :create
@@ -17,5 +9,20 @@ class User < ActiveRecord::Base
   validates_length_of :full_name, :minimum => 10, :message => 'Tu nombre debe tener por lo menos 3 caracteres.', on:  :create
   validates_format_of :full_name, :with => /\A[a-zA-Z áéíóúÁÉÍÓÚñÑ]+\z/, :message => "El nombre solo debe tener letras.", on:  :create
 
+  validates_presence_of :state ,  :message => "Indica tu Estado",  if: :deputation_is_blank?
+  validates_presence_of :municipality ,  :message => "Indica tu municipio", if: :deputation_is_blank_and_state_is_fill?
+  validates_presence_of :deputation ,  :message => "Indica tu delegación", if: :state_is_blank?
 
+  def deputation_is_blank?
+    deputation.nil? || deputation.blank?
+  end
+
+  def deputation_is_blank_and_state_is_fill?
+    deputation.nil? || deputation.blank? && !state.nil? && !state.empty?
+  end
+
+  def state_is_blank?
+    return state.nil? || state.blank?
+  end
 end
+
