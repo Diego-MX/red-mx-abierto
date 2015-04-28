41f55c12725501e95533bba7560cb376e25eef44
mikesaurio <mikesaurio@gmail.com>
2015-04-27 19:57:32 -0500

    se agregan traducciones a User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :dependency_name, :message => 'Debes escribir el nombre de la dependencia', on:  :create
-  validates_length_of :dependency_name, :minimum => 10, :message => 'El nombre de la dependencia debe tener por lo menos 3 caracteres.', on:  :create
+  validates_presence_of :dependency_name, :message => I18n.t('user.validation.dependency_name_empty') , on:  :create
+  validates_length_of :dependency_name, :minimum => 10, :message => I18n.t('user.validation.dependency_name_length'), on:  :create
 
-  validates_presence_of :full_name, :message => "Tu nombre  no puede estar en blanco.", on:  :create
-  validates_length_of :full_name, :minimum => 10, :message => 'Tu nombre debe tener por lo menos 3 caracteres.', on:  :create
-  validates_format_of :full_name, :with => /\A[a-zA-Z áéíóúÁÉÍÓÚñÑ]+\z/, :message => "El nombre solo debe tener letras.", on:  :create
+  validates_presence_of :full_name, :message => I18n.t('user.validation.dependency_name_empty'), on:  :create
+  validates_length_of :full_name, :minimum => 10, :message => I18n.t('user.validation.full_name_length'), on:  :create
+  validates_format_of :full_name, :with => /\A[a-zA-Z áéíóúÁÉÍÓÚñÑ]+\z/, :message => I18n.t('user.validation.full_name_format'), on:  :create
 
-  validates_presence_of :government_type ,  :message => "Indica el tipo de gobierno al que perteneces"
-  validates_presence_of :state ,  :message => "Indica tu Estado",  if: :deputation_is_blank?
-  validates_presence_of :deputation ,  :message => "Indica tu delegación", if: :state_is_blank?
+  validates_presence_of :government_type ,  :message => I18n.t('user.validation.government_type_empty')
+  validates_presence_of :state ,  :message => I18n.t('user.validation.state_empty'),  if: :deputation_is_blank?
+  validates_presence_of :deputation ,  :message => I18n.t('user.validation.deputation_empty'), if: :state_is_blank?
 
   def deputation_is_blank?
     deputation.nil? || deputation.blank?
