4bad7fdc689836fec4b3bb96c8ab97c20c3040ba
Raúl Jiménez <jimenezjrs@gmail.com>
2015-04-28 12:34:11 -0500

    - Created stages policies - Removed ids from seed files - Added admin authentication for admin/stages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,32 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  include Pundit
   protect_from_forgery with: :exception
+  # let's verify that authorization is been handled for every view
+  after_action :verify_authorized, :except => :index
+
+  def authenticate_admin!
+    current_user.admin? or not_found
+  end
 
   def after_sign_in_path_for(resource)
-    stages_path
+    if resource.admin?
+      admin_stages_path
+    else
+      stages_path
+    end
   end
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  def verify_authorized
+    super unless devise_controller?
+  end
+
+  private
+
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
   end
 end
