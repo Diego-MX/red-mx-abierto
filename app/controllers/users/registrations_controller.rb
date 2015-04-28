3ea3f450a338020bae2ae993f3e52d15287d9688
mikesaurio <mikesaurio@gmail.com>
2015-04-28 14:47:59 -0500

    se crea un helper para visualizar los text_field que se ocupan sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,58 +1,33 @@
-module Devise
-  class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :set_values
+  module Devise
+    class Users::RegistrationsController < Devise::RegistrationsController
+      helper_method :deputations_options, :states_options
 
-    def new
-      @user = User.new
-    end
-    
-    def create
-      if conditions_accepted?
-            type_of_officer
-              @user = User.new(user_params) 
-              if @user.save 
-                redirect_to root_path,  notice:  I18n.t('messages.user_create') 
-              else
-                render :new
-              end
+      def new
+        @user = User.new
       end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name, :accept)
-  end
-
-  def conditions_accepted?
-    params[:accept].each do |key, value|
-      if value == '1' 
-      @acepted = true 
-    else
-      @acepted = false 
-    end
-    end 
-    return  !params[:user].nil?
-    end
-
-  def set_values
-    @deputations= ['Alvaro Obregon' ,  'Azcapotzalco','Benito Juarez', 'Cuajuimalpa', 'Coyoacan', 'Cuahutemoc', 'Gustavo A Madero', 'Iztacalco',
-      'Iztapalapa', 'Magdalena Contreras', 'Miguel Hidalgo', 'Milpa Alta', 'Tlahuac', 'Tlalpan', 'Venustiano Carranza', 'Xochimilco']
-
-      @states= ['Aguascalientes','Baja California Norte', 'Baja California Sur ','Campeche ','Chiapas ','Chihuahua', 'Coahuila','Colima ', 'Durango', 'Guanajuato', 
-        'Guerrero  ', 'Hidalgo ','Guadalajara','Edo. México ','Michoacán ','Morelos ','Nayarit','Monterrey','Oaxaca', 'Puebla ','Querétaro', 'Quintana Roo', 
-        'San Luís Potosí', 'Sinaloa ', 'Sonora ', 'Tabasco',  'Tamaulipas', 'Tlaxcala ', 'Veracruz', 'Yucatán ', 'Zacatecas', 'Distrito Federal' ]
-  end
-
-      def type_of_officer
-        @type_government = params[:user][:government_type]
-        if @type_government  == 'delegacion'
-          @delegacional_cheked = true
-        elsif @type_government  == 'municipal'
-          @municional_cheked = true
-        elsif @type_government  == 'estatal'
-          @estatal_cheked = true
-        end
+      
+      def create
+          @user = User.new(user_params) 
+          if @user.save 
+            redirect_to root_path,  notice:  I18n.t('messages.user_create') 
+          else
+            render :new
+          end
       end
-end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email,   :password, :password_confirmation, :government_type, :state, :municipality, :deputation, :dependency_name, :full_name, :accept)
+      end
+
+
+     def deputations_options
+      UserRegistrations.deputations_options
+    end
+
+    def states_options
+      UserRegistrations.states_options
+    end
+  end
 end
