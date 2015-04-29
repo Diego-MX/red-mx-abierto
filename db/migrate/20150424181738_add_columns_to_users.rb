b6c5378304774096978c702af8429cd97bcccde5
mikesaurio <mikesaurio@gmail.com>
2015-04-29 11:55:45 -0500

    se modifica tipo text por String y se elimina migracion

--- db/migrate/20150424181738_add_columns_to_users.rb
+++ db/migrate/20150424181738_add_columns_to_users.rb
@@ -1,10 +1,10 @@
 class AddColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :government_type, :text
-    add_column :users, :state, :text
-    add_column :users, :municipality, :text
-    add_column :users, :deputation, :text
-    add_column :users, :dependency_name, :text
-    add_column :users, :full_name, :text
+    add_column :users, :government_type, :string
+    add_column :users, :state, :string
+    add_column :users, :municipality, :string
+    add_column :users, :deputation, :string
+    add_column :users, :dependency_name, :string
+    add_column :users, :full_name, :string
   end
 end
