339f3766a087179a91214aa25222a25be32294f0
mikesaurio <mikesaurio@gmail.com>
2015-04-30 13:39:20 -0500

    cuando el usuario se registra se envia al sign in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@
       def create
           @user = User.new(user_params) 
           if @user.save 
-            redirect_to root_path,  notice:  I18n.t('messages.user_create') 
+            redirect_to user_session_path,  notice:  I18n.t('messages.user_create') 
           else
             render :new
           end
