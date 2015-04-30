a9e9dfec5ee8a0c6fb8f561f31a7aad9715282a7
mikesaurio <mikesaurio@gmail.com>
2015-04-29 19:30:56 -0500

    se crea filtro de la categoria de gobierno

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,13 +1,32 @@
 class MembersController < ApplicationController
-   helper_method :type_of_members
-      layout 'devise'
-  def index
-    @user = User.all
-  end
+ helper_method :type_of_members
+ layout 'devise'
 
-  
+ def index
+  set_user_filter
+end
+
+
   private
-  def type_of_members
+    def type_of_members
       ShowMembers.type_of_members
     end
-end
+
+    def set_user_filter
+      unless params[:post].nil?
+        type = params[:post][:government_type]
+        if type == 'Delegacional'
+          @user = User.where(government_type: 'delegacion')
+        elsif type == 'Municipal'
+          @user = User.where(government_type: 'municipal')
+        elsif type == 'Estatal'
+          @user = User.where(government_type: 'estatal')
+        else
+          @user = User.all
+        end#termina if
+      else
+        @user = User.all
+      end#termina unless
+    end#termina def
+  end
+
--- app/services/show_members.rb
+++ app/services/show_members.rb
@@ -1,5 +1,5 @@
 module ShowMembers 
   def self.type_of_members
-   [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5]]
+    { 1 => "Estatal", 2 => "Municipal", 3 => "Delegacional"}
   end
 end 
