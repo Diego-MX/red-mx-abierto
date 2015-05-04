d5a20c4bcf1d46b053818e46f7172477c4e46547
Raúl Jiménez <jimenezjrs@gmail.com>
2015-05-04 18:04:10 -0500

    basic implementation guide layout implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def header_background
     (user_signed_in? && current_user.admin?) ? 'admin-background' : 'azulado'
   end
+
+  def no_left_border(idx)
+    "no-left-border" if idx == 0
+  end
 end
